1adc45f9cf6a3d9dfcb7faec478a6717c8dedef1
Caio Mori <caiomori22@gmail.com>
2025-01-17 18:16:54 -0300

    Fix podspec source_files tag

--- MBLoan.podspec
+++ MBLoan.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'MBLoan'
-  s.version          = '0.1.5'
+  s.version          = '0.1.6'
   s.summary          = 'A simple and efficient loan calculation library for Swift.'
 
   s.description      = <<-DESC
---
+++ MBLoan/Classes/MBLoanView.swift
@@ -0,0 +1,18 @@
+//
+//  MBLoanView.swift
+//  MBLoan
+//
+//  Created by Caio Mori on 17/01/25.
+//
+
+import SwiftUI
+
+struct MBLoanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MBLoanView()
+}
