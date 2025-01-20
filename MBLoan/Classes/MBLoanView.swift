fb72b8373f38826b5c40a9820c115d265d69a6b0
Caio Mori <caiomori22@gmail.com>
2025-01-20 14:09:19 -0300

    feat: add new view

--- MBLoan/Classes/MBLoanView.swift
+++ MBLoan/Classes/MBLoanView.swift
@@ -7,9 +7,63 @@
 
 import SwiftUI
 
-struct MBLoanView: View {
+public struct MBLoanView: View {
+    
+    // MARK: - UI Components
+    
+    private let backgroundColor = Color(red: 240.0/255.0,
+                                        green: 240.0/255.0,
+                                        blue: 240.0/255.00)
+    
+    private let yellowColor = Color(red: 255.0/255.0,
+                                    green: 186.0/255.0,
+                                    blue: 5.0/255.0)
+    
+    // MARK: - Init
+    
+    public init(){}
+    
+    // MARK: - View
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .top) {
+            LinearGradient(
+                gradient: Gradient(colors: [yellowColor, backgroundColor]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            
+            VStack {
+                headerView()
+                    .padding(.leading, 20)
+            }
+        }
+    }
+    
+    var headerView: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Empréstimo")
+                    .font(.system(size: 24))
+                    .foregroundStyle(.black)
+                    .fontWeight(.light)
+                
+                Text("MoriBank")
+                    .font(.system(size: 24))
+                    .foregroundStyle(.black)
+                    .fontWeight(.light)
+                
+                Spacer()
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.top)
+            .padding(.bottom, 3)
+            
+            Text("Antecipe seus planos")
+                .font(.system(size: 24))
+                .foregroundStyle(.black)
+                .fontWeight(.light)
+        }
     }
 }
 
